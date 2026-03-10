6b8bdfb16033103d06b22bf798b0ebc632977b31
Cong Luan Tran <congluan.tran19@gmail.com>
2026-03-10 15:53:01 +0700

    feat: add copilot

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'github/copilot.vim',
+    options = {},
+  },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
+    },
+    build = 'make tiktoken',
+    opts = {
+      -- See Configuration section for options
+    },
+  },
+}
