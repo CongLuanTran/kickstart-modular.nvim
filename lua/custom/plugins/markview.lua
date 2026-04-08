88bddfdc541e90e213538db4721a13eb2df4bc67
Cong Luan Tran <congluan.tran19@gmail.com>
2026-04-09 00:46:58 +0700

    fix: re-enable typst render of markview

--- lua/custom/plugins/markview.lua
+++ lua/custom/plugins/markview.lua
@@ -2,11 +2,7 @@
 return {
   'OXY2DEV/markview.nvim',
   lazy = false,
-  opts = {
-    typst = {
-      enable = false,
-    },
-  },
+  opts = {},
 
   -- Completion for `blink.cmp`
   dependencies = { 'saghen/blink.cmp' },
