91a6910f1fdddfe4504bdf29645cf3171e20a062
Cong Luan Tran <congluan.tran19@gmail.com>
2026-04-28 10:00:51 +0700

    feat: include omnifunc in blink-cmp

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -72,7 +72,7 @@ return {
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'omni', 'path', 'snippets', 'lazydev' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         },
