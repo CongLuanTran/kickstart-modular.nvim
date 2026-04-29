212b53fccaba0ec9d31ed2f578ae2886dcba5d76
Cong Luan Tran <congluan.tran19@gmail.com>
2026-04-29 10:44:45 +0700

    fix: revert markdown-oxide refactor

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -153,6 +153,33 @@ return {
         gopls = {},
       }
 
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('lsp_attach_markdown_oxide', { clear = true }),
+        callback = function(ev)
+          local client = vim.lsp.get_client_by_id(ev.data.client_id)
+          if client == nil then return end
+          if client.name == 'markdown_oxide' then
+            vim.api.nvim_create_user_command('Daily', function(args)
+              local input = args.args
+              client:exec_cmd { title = 'Daily note', command = 'jump', arguments = { input } }
+            end, { desc = 'Open daily note', nargs = '*' })
+          end
+        end,
+      })
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      vim.lsp.config('markdown_oxide', {
+        capabilities = vim.tbl_deep_extend('force', capabilities, {
+          workspace = {
+            didChangeWatchedFiles = {
+              dynamicRegistration = true,
+            },
+          },
+        }),
+      })
+
+      vim.lsp.enable 'markdown_oxide'
+
       -- I have a feeling that this will be buggy
       -- But let's see how it goes
       vim.lsp.config('harper_ls', {
