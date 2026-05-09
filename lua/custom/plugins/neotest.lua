3c11c132d76e5530860a22da663872b62f314312
Cong Luan Tran <congluan.tran19@gmail.com>
2026-05-09 16:38:53 +0700

    feat: update neotest parameter

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -6,13 +6,20 @@ return {
     'antoinemadec/FixCursorHold.nvim',
     'nvim-treesitter/nvim-treesitter',
     'nvim-neotest/neotest-python',
+    'nvim-neotest/neotest-plenary',
+    'nvim-neotest/neotest-vim-test',
   },
   config = function()
+    ---@diagnostic disable: missing-fields
     require('neotest').setup {
       adapters = {
         require 'neotest-python' {
           dap = { justMyCode = false },
           pytest_discover_instances = true,
+        },
+        require 'neotest-plenary',
+        require 'neotest-vim-test' {
+          ignore_file_types = { 'python', 'vim', 'lua' },
         },
       },
     }
