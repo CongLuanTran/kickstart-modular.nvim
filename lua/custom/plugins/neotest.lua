a97d62258da7e64cb0fd70a53870902e204eb1f7
Cong Luan Tran <congluan.tran19@gmail.com>
2026-05-11 13:00:13 +0700

    feat: add test setup for rust

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -21,6 +21,7 @@ return {
         require 'neotest-vim-test' {
           ignore_file_types = { 'python', 'vim', 'lua' },
         },
+        require 'rustaceanvim.neotest',
       },
     }
   end,
