1490b5b7c92bb0e3b3b697a1665bab1ee9eb2232
Cong Luan Tran <congluan.tran19@gmail.com>
2026-05-12 23:39:40 +0700

    refactor: move colorscheme into a gitignored file

--- init.lua
+++ init.lua
@@ -106,7 +106,8 @@ require 'lazy-bootstrap'
 require 'lazy-plugins'
 
 -- [[ Set colorscheme ]]
-vim.cmd 'colorscheme rose-pine'
+-- The actual colorscheme file will be ignored by git
+require 'colorscheme'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
