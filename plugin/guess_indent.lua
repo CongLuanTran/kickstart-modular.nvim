06c54c5a2f0d0a812bc6d302f2b34ec7e41f433c
Cong Luan Tran <congluan.tran19@gmail.com>
2026-05-24 11:03:52 +0700

    fix: setup guess-indent

--- plugin/guess_indent.lua
+++ plugin/guess_indent.lua
@@ -2,4 +2,6 @@ vim.pack.add {
   'https://github.com/NMAC427/guess-indent.nvim',
 }
 
+require('guess-indent').setup {}
+
 vim.keymap.set('n', '<leader>gi', function() require('guess-indent').set_indent() end, { desc = 'Guess indent' })
