e28deeac6048ae0cd7e9c6a143fe5f1f37a4e53d
Cong Luan Tran <congluan.tran19@gmail.com>
2026-06-09 22:19:38 +0700

    feat: make neovim harder

---
+++ plugin/hardtime.lua
@@ -0,0 +1,10 @@
+vim.pack.add {
+  'https://github.com/MunifTanjim/nui.nvim',
+  'https://github.com/m4xshen/hardtime.nvim',
+}
+
+require('hardtime').setup {
+  disabled_filetypes = {
+    ['nvim-pack'] = true,
+  },
+}
