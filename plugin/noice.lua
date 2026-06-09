0263861fa4a5ae3018021b9de2e3e73e29871200
Cong Luan Tran <congluan.tran19@gmail.com>
2026-06-09 22:16:35 +0700

    refactor: minor changes to noice

--- plugin/noice.lua
+++ plugin/noice.lua
@@ -20,8 +20,8 @@ noice.setup {
     bottom_search = true, -- use a classic bottom cmdline for search
     command_palette = true, -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = true, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = true, -- add a border to hover docs and signature help
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
   },
 
   routes = {
@@ -38,6 +38,6 @@ noice.setup {
 local map = vim.keymap.set
 
 map('n', '<leader>nl', function() noice.cmd 'last' end, { desc = '[L]ast [N]otification' })
-map('n', '<leader>nh', function() noice.require('noice').cmd 'history' end, { desc = '[N]otification [H]istory' })
+map('n', '<leader>nh', function() noice.cmd 'history' end, { desc = '[N]otification [H]istory' })
 map('n', '<leader>na', function() noice.cmd 'all' end, { desc = '[A]ll [N]otifications' })
 map('n', '<leader>nd', function() noice.cmd 'dismiss' end, { desc = '[D]ismiss All [N]otifications' })
