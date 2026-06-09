b6f3d5083d29f3e1b3ed764946a7604fc76fb841
Cong Luan Tran <congluan.tran19@gmail.com>
2026-06-10 00:02:03 +0700

    fix: source vim.g.rustaceanvim before loading rust plugins

--- init.lua
+++ init.lua
@@ -102,5 +102,7 @@ require 'colorscheme'
 -- [[ Emacs Keymap]]
 require 'emacs'
 
+-- [[ Rustaceanvim ]]
+require 'rustaceanvim'
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- plugin/rust.lua
+++ plugin/rust.lua
@@ -8,43 +8,6 @@ vim.pack.add {
   'https://github.com/Saecki/crates.nvim',
 }
 
-vim.g.rustaceanvim = {
-  server = {
-    on_attach = function(_, bufnr)
-      vim.keymap.set('n', 'gra', function() vim.cmd.RustLsp 'codeAction' end, { desc = '[G]oto Code [A]ction', buffer = bufnr, silent = true })
-      vim.keymap.set('n', 'K', function() vim.cmd.RustLsp { 'hover', 'actions' } end, { silent = true, buffer = bufnr })
-    end,
-    default_settings = {
-      -- rust-analyzer language server configuration
-      ['rust-analyzer'] = {
-        cargo = {
-          allFeatures = true,
-          loadOutDirsFromCheck = true,
-          buildScripts = { enable = true },
-        },
-        checkOnSave = false,
-        diagnostics = { enable = false },
-        procMacro = { enable = true },
-        files = {
-          exclude = {
-            '.direnv',
-            '.git',
-            '.jj',
-            '.github',
-            '.gitlab',
-            'bin',
-            'node_modules',
-            'target',
-            'venv',
-            '.venv',
-          },
-          watcher = 'client',
-        },
-      },
-    },
-  },
-}
-
 local crates_loaded = false
 vim.api.nvim_create_autocmd('BufRead', {
   pattern = 'Cargo.toml',
